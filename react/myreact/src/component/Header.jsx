37b112b64a0fd2b9379f2b7eb06b2e01959aa39b
abhinandan-savale <abhinandansavale777@gmail.com>
2024-02-19 23:47:44 +0530

    I have created a simple dashboard using React JS

---
+++ react/myreact/src/component/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <img src='jj.png' />
+        <div className='header1'>
+        <img src='jj.png' />
+        </div>
+        
+        </div>
+       
+  )
+}
+
+export default Header
